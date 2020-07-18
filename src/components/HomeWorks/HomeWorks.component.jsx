49e56f1c96c9a3c582affa15526a78908f1b8055
Maeva Wolff <maeva.wolff@hetic.net>
2020-07-18 20:09:49 +0200

    create home works component

---
+++ src/components/HomeWorks/HomeWorks.component.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import works from '../../data/works.json';
+
+export default class HomeWorks extends Component {
+  render() {
+    return (
+      <div className='homeWorks'>
+        {works.map(work => {
+          return (
+            <div className='homeWorks__item'>
+              <h3 className='homeWorks__name'>{work.name}</h3>
+              <h4 className='homeWorks__year'>{work.year}</h4>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
--- src/pages/HomePage/Home.page.jsx
+++ src/pages/HomePage/Home.page.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import GridsBackground from '../../components/GridsBackground/GridsBackground.component';
 import HomeHero from '../../components/HomeHero/HomeHero.component';
+import HomeWorks from '../../components/HomeWorks/HomeWorks.component';
 
 export default class HomePage extends Component {
   render() {
@@ -9,6 +10,7 @@ export default class HomePage extends Component {
       <div className='home'>
         <GridsBackground />
         <HomeHero />
+        <HomeWorks />
       </div>
     );
   }
