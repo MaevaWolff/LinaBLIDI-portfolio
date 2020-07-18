b071c249fc4eef568660354570c4add9c4d41a10
Maeva Wolff <maeva.wolff@hetic.net>
2020-07-18 18:06:28 +0200

    test of works data recovery in json file

--- src/pages/WorksPage/Works.page.jsx
+++ src/pages/WorksPage/Works.page.jsx
@@ -1,7 +1,24 @@
 import React, { Component } from 'react';
+import works from '../../data/works.json';
 
 export default class WorksPage extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div className='works'>
+        {works.map((el, index) => {
+          return (
+            <div key={index} className='works-items'>
+              <ul>
+                {el.partners.map((partner, i) => {
+                  return <li key={i}>{partner}</li>;
+                })}
+              </ul>
+
+              <p>{el.name}</p>
+            </div>
+          );
+        })}
+      </div>
+    );
   }
 }
