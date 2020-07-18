5e137ae945334b579304d5b7727d2d43be763f33
Maeva Wolff <maeva.wolff@hetic.net>
2020-07-19 00:14:38 +0200

    add link for redirect works

--- src/components/HomeWorks/HomeWorks.component.jsx
+++ src/components/HomeWorks/HomeWorks.component.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import works from '../../data/works.json';
 
 export default class HomeWorks extends Component {
@@ -7,10 +8,16 @@ export default class HomeWorks extends Component {
       <div className='homeWorks'>
         {works.map((work, index) => {
           return (
-            <div key={index} className='homeWorks__item'>
-              <h3 className='homeWorks__name'>{work.name}</h3>
-              <h4 className='homeWorks__year'>({work.year})</h4>
-            </div>
+            <Link
+              className='homeWorks__container'
+              key={index}
+              to={`/works?id=${work.name}`}
+            >
+              <div className='homeWorks__item'>
+                <h3 className='homeWorks__name'>{work.name}</h3>
+                <h4 className='homeWorks__year'>({work.year})</h4>
+              </div>
+            </Link>
           );
         })}
       </div>
