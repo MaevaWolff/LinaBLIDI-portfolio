bf936f2b3e8ad945a52717a247d4eceee2b99dad
Maeva Wolff <maeva.wolff@hetic.net>
2020-07-20 20:01:28 +0200

    add an alert to inform user of wip

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import './scss/styles.scss';
 import HomePage from './pages/HomePage/Home.page';
 import WorksPage from './pages/WorksPage/Works.page';
 import Header from './components/Header/Header.component';
+import Alert from './components/Alert/Alert.component';
 
 function App() {
   return (
     <Router>
       <div className='App'>
+        <Alert AlertText='🚧 WORK IN PROGRESS 🚧 ' AlertType='danger' />
         <Header />
         <Switch>
           <Route exact path='/' component={HomePage} />
