c67cb2d6c4606df9d7b1ff798881952e12a5616e
Maeva Wolff <maeva.wolff@hetic.net>
2020-07-23 00:46:06 +0200

    add mouseover and mouseleave functions on works home to add class on body

--- src/components/HomeWorks/HomeWorks.component.jsx
+++ src/components/HomeWorks/HomeWorks.component.jsx
@@ -3,6 +3,40 @@ import { Link } from 'react-router-dom';
 import works from '../../data/works.json';
 
 export default class HomeWorks extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      socoda: 'socoda',
+      coquette: 'coquette',
+      canwebreathenow: 'canwebreathenow?',
+      mypocket: 'mypocket',
+      gallery: '3d gallery'
+    };
+  }
+  handleMouseOver = event => {
+    console.log(JSON.parse(event.target.dataset.info));
+    const body = document.body;
+    const data = JSON.parse(event.target.dataset.info);
+    if (data === this.state.socoda) {
+      body.classList.add('socoda');
+    } else if (data === this.state.coquette) {
+      body.style.backgroundColor = 'purple';
+    } else if (data === this.state.coquette) {
+      body.style.backgroundColor = 'purple';
+    } else if (data === this.state.canwebreathenow) {
+      body.style.backgroundColor = 'palevioletred';
+    } else if (data === this.state.mypocket) {
+      body.style.backgroundColor = 'blue';
+    } else if (data === this.state.gallery) {
+      body.style.backgroundColor = 'yellow';
+    }
+  };
+
+  handleMouseLeave = () => {
+    const body = document.body;
+    body.style.backgroundColor = '';
+    body.classList = '';
+  };
   render() {
     return (
       <div className='homeWorks'>
@@ -13,7 +47,12 @@ export default class HomeWorks extends Component {
               key={index}
               to={`/works?name=${work.name}`}
             >
-              <div className='homeWorks__item'>
+              <div
+                data-info={JSON.stringify(work.name)}
+                onMouseOver={this.handleMouseOver}
+                onMouseLeave={this.handleMouseLeave}
+                className='homeWorks__item'
+              >
                 <h3 className='homeWorks__name'>{work.name}</h3>
                 <h4 className='homeWorks__year'>({work.year})</h4>
               </div>
