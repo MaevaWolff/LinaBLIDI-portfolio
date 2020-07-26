18d4c71d23191272a1480a40d754319936090218
Maeva Wolff <maeva.wolff@hetic.net>
2020-07-26 16:08:03 +0200

    convert alter scss style to styled component

--- src/components/Alert/Alert.component.jsx
+++ src/components/Alert/Alert.component.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import styled from 'styled-components';
+import { colors } from '../../styles/config/const';
+
+const Container = styled.div`
+  position: fixed;
+  z-index: 6;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: grey;
+  width: 100%;
+  height: 40px;
+  color: ${colors.neutralBlack};
+  font-family: Assistant;
+  font-weight: 600;
+
+  &.danger {
+    background-color: rgb(255, 172, 172);
+    color: rgb(190, 38, 38);
+  }
+`;
+
 export default function Alert(props) {
   return (
-    <div className={`alert ${props.AlertType}`}>
-      <p className='alert__text'>{props.AlertText}</p>
-    </div>
+    <Container className={`${props.AlertType}`}>
+      <p>{props.AlertText}</p>
+    </Container>
   );
 }
 
