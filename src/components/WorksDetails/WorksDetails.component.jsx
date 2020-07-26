d44556c361ba3f00881d44aae3e05d4f989a04e9
Maeva Wolff <maeva.wolff@hetic.net>
2020-07-26 15:29:14 +0200

    convert works details scss style to styled component

--- src/components/WorksDetails/WorksDetails.component.jsx
+++ src/components/WorksDetails/WorksDetails.component.jsx
@@ -1,46 +1,44 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import {
+  colors,
+  breakpoints,
+  fontSize,
+  titleFontSize
+} from '../../styles/config/const';
 
 import WorksDetailsTitle from './WorksDetailsTitle.component';
 
 export default function WorksDetails(props) {
   return (
-    <div className='worksDetails'>
-      <h2 className='worksDetails__title'>PROJECT DETAILS</h2>
-      <p className='worksDetails__time'>{props.worksTime}</p>
-      <p className='worksDetails__date'>{props.worksDate}</p>
+    <Container>
+      <Title>PROJECT DETAILS</Title>
+      <Time>{props.worksTime}</Time>
+      <Date>{props.worksDate}</Date>
 
-      <section className='worksDetails__categories'>
-        <article className='worksDetails__category'>
+      <Categories>
+        <Category>
           <WorksDetailsTitle worksDetailsTitle='Title' />
-          <p className='worksDetails__name'>{props.worksName}</p>
-        </article>
-        <article className='worksDetails__category'>
+          <Name>{props.worksName}</Name>
+        </Category>
+        <Category>
           <WorksDetailsTitle worksDetailsTitle='Colors' />
-          <div className='worksDetails__colors'>
-            <div
-              className='worksDetails__color'
-              style={{ backgroundColor: props.worksColor1 }}
-            ></div>
-            <div
-              className='worksDetails__color'
-              style={{ backgroundColor: props.worksColor2 }}
-            ></div>
-            <div
-              className='worksDetails__color'
-              style={{ backgroundColor: props.worksColor3 }}
-            ></div>
-          </div>
-        </article>
-        <article className='worksDetails__category'>
+          <Colors>
+            <Color style={{ backgroundColor: props.worksColor1 }}></Color>
+            <Color style={{ backgroundColor: props.worksColor2 }}></Color>
+            <Color style={{ backgroundColor: props.worksColor3 }}></Color>
+          </Colors>
+        </Category>
+        <Category>
           <WorksDetailsTitle worksDetailsTitle='Typography' />
-        </article>
-        <article className='worksDetails__category'>
+        </Category>
+        <Category>
           <WorksDetailsTitle worksDetailsTitle='Partners' />
-          <ul className='worksDetails__partners'>{props.worksPartners}</ul>
-        </article>
-      </section>
-    </div>
+          <Partners>{props.worksPartners}</Partners>
+        </Category>
+      </Categories>
+    </Container>
   );
 }
 
@@ -57,3 +55,82 @@ WorksDetails.propTypes = {
 WorksDetails.defaultProps = {
   worksName: 'project name'
 };
+
+const Container = styled.div`
+  text-transform: uppercase;
+  margin: 0 4%;
+  @include medium {
+    margin: 0 10%;
+  }
+`;
+
+const Title = styled.h2`
+  width: fit-content;
+  border-bottom: 1px solid ${colors.neutralBlack};
+  padding-bottom: 4px;
+  font-size: 23px;
+  @media (min-width: ${breakpoints.medium}) {
+    font-size: 40px;
+  }
+`;
+
+const Time = styled.p`
+  font-family: Assistant;
+  margin-top: 20px;
+  @media (min-width: ${breakpoints.medium}) {
+    font-size: ${fontSize.m};
+    margin-top: 40px;
+  }
+`;
+
+const Date = styled.p`
+  font-family: Assistant;
+  width: 100%;
+  border-bottom: 1px solid ${colors.neutralBlack};
+  margin-top: 10px;
+  padding-bottom: 20px;
+  @media (min-width: ${breakpoints.medium}) {
+    padding-bottom: 40px;
+    font-size: ${fontSize.m};
+  }
+`;
+
+const Categories = styled.section`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 1fr;
+  margin: 8% 4%;
+  grid-gap: 40px;
+  @media (min-width: ${breakpoints.medium}) {
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 1fr 1fr;
+  }
+`;
+
+const Category = styled.article``;
+
+const Name = styled.p`
+  margin-top: 4%;
+  font-family: Assistant;
+  font-weight: 600;
+  font-size: ${titleFontSize.s};
+`;
+
+const Colors = styled.div`
+  margin-top: 4%;
+  display: flex;
+  justify-content: space-evenly;
+`;
+
+const Color = styled.div`
+  width: 60px;
+  height: 60px;
+  @media (min-width: ${breakpoints.medium}) {
+    width: 80px;
+    height: 80px;
+  }
+`;
+
+const Partners = styled.ul`
+  margin-top: 4%;
+`;
--- src/components/WorksDetails/WorksDetailsTitle.component.jsx
+++ src/components/WorksDetails/WorksDetailsTitle.component.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { colors, breakpoints } from '../../styles/config/const';
 
-export default function WorksDetailsTitle(props) {
-  return <div className='worksDetailsTitle'>{props.worksDetailsTitle}</div>;
+const Title = styled.h3`
+  border-bottom: 1px solid ${colors.neutralBlack};
+  padding-bottom: 4px;
+  font-family: Assistant;
+  @media (min-width: ${breakpoints.medium}) {
+    font-size: 18px;
+  }
+`;
+
+export default function WorksDetailsTitle({ worksDetailsTitle }) {
+  return <Title>{worksDetailsTitle}</Title>;
 }
 
 WorksDetailsTitle.propTypes = {
