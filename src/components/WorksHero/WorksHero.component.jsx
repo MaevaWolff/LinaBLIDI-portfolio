97dde0c1a305b8201ecb4851e81feac55db40e0f
Maeva Wolff <maeva.wolff@hetic.net>
2020-07-26 15:40:29 +0200

    convert works hero scss style to styled component

--- src/components/WorksHero/WorksHero.component.jsx
+++ src/components/WorksHero/WorksHero.component.jsx
@@ -1,7 +1,88 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { colors, breakpoints, fontSize } from '../../styles/config/const';
 
 import IndicatorScroll from '../IndicatorScroll/IndicatorScroll.component';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const Infos = styled.div`
+  margin: 0 10%;
+  @media (min-width: ${breakpoints.medium}) {
+    margin: 0 120px;
+  }
+  @media (min-width: ${breakpoints.large}) e {
+    margin: 0 200px;
+  }
+`;
+
+const Name = styled.h1`
+  text-transform: uppercase;
+  font-size: 23px;
+  @media (min-width: ${breakpoints.medium}) {
+    font-size: 70px;
+  }
+`;
+
+const Hashtag = styled.p`
+  font-weight: 600;
+  font-size: ${fontSize.s};
+  text-transform: uppercase;
+  color: ${colors.primaryColor};
+  @media (min-width: ${breakpoints.medium}) {
+    font-size: ${fontSize.m};
+  }
+`;
+
+const Description = styled.p`
+  font-family: 'Assistant';
+  width: 70%;
+  margin-top: 45px;
+  font-size: ${fontSize.s};
+  line-height: 20px;
+  text-transform: uppercase;
+  @media (min-width: ${breakpoints.medium}) {
+    font-size: ${fontSize.m};
+    margin-top: 60px;
+    line-height: 26px;
+  }
+`;
+
+const Link = styled.a`
+color: var(--primary-color);
+text-decoration: underline;
+font-weight: 600;
+p {
+  margin-top: 80px;
+  @media (min-width: ${breakpoints.medium}) {
+    font-size: 18px;
+    margin-top: 20px;
+  }
+  @media (min-width: ${breakpoints.large}){
+    margin-top: 40px;
+  }
+}}
+`;
+
+// const Image = styled.img`
+//   position: absolute;
+//   z-index: -1;
+//   right: -140px;
+//   max-width: 90%;
+//   max-height: 60%;
+//   transform: rotate(18deg);
+//   @media (min-width: ${breakpoints.medium}) {
+//     transform: rotate(8deg);
+//     right: -40px;
+//   }
+// `;
 
 export default function WorksHero(props) {
   useEffect(() => {
@@ -11,23 +92,22 @@ export default function WorksHero(props) {
   });
 
   return (
-    <div className='worksHero'>
+    <Container>
       <IndicatorScroll />
-      <div className='worksHero__infos'>
-        <h1 className='worksHero__name'>{props.worksName}</h1>
-        <p className='worksHero__hashtag'>#{props.worksHashtag}</p>
-        <p className='worksHero__description'>{props.worksDescription}</p>
+      <Infos>
+        <Name>{props.worksName}</Name>
+        <Hashtag>#{props.worksHashtag}</Hashtag>
+        <Description>{props.worksDescription}</Description>
 
-        <a href={props.worksLink} className='worksHero__link'>
+        <Link href={props.worksLink}>
           <p>see the website right here</p>
-        </a>
-      </div>
-      {/* <img
-          className='worksHero__image'
+        </Link>
+      </Infos>
+      {/* <Image
           src={require(`../../assets/${this.props.worksHero}`)}
           alt='image_hero_project'
         /> */}
-    </div>
+    </Container>
   );
 }
 
