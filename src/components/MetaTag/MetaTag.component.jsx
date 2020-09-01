a2c46b584c2cd9181c25e8a2e01c1840271b2d3a
Maeva Wolff <maeva.wolff@hetic.net>
2020-09-01 23:05:57 +0200

    fix meta description on socials

--- src/components/MetaTag/MetaTag.component.jsx
+++ src/components/MetaTag/MetaTag.component.jsx
@@ -9,17 +9,13 @@ export default function MetaTag(props) {
       <title>
         {`LINA BLIDI - DESIGNER UI/UX FREELANCE - ${props.metatagPage}`}
       </title>
-      <meta name="description" content="Hey, I'm Lina Blidi a UI UX Designer, student and freelance from Paris :relaxed:" />
 
       <meta property="og:type" content="website"></meta>
       <meta property="og:url" content="https://linablidi.netlify.app/" />
-      <meta property="og:title" content="Lina Blidi Portfolio"></meta>
-      <meta property="og:description" content="Hey, I'm Lina Blidi a UI UX Designer, student and freelance from Paris :relaxed:"></meta>
 
       <meta property="twitter:card" content="summary_large_image"></meta>
       <meta property="twitter:url" content="https://linablidi.netlify.app/" />
-      <meta property="twitter:title" content="Lina Blidi Portfolio"></meta>
-      <meta property="twitter:description" content="Hey, I'm Lina Blidi a UI UX Designer, student and freelance from Paris :relaxed:"></meta>
+
 
     </Helmet>
   );
