5647b11157fde96ac84c2116babbd109f627aac9
Maeva Wolff <maeva.wolff@hetic.net>
2020-09-01 22:44:09 +0200

    test fix meta tag

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './scss/styles.scss';
+import cover from './assets/cover.png'
 
 import HomePage from './pages/HomePage/Home.page';
 import WorksPage from './pages/WorksPage/Works.page';
@@ -12,6 +14,21 @@ function App() {
   return (
     <Router>
       <div className='App'>
+        <Helmet>
+          <meta name="description" content="Hey, I'm Lina Blidi a UI UX Designer, student and freelance from Paris :relaxed:" />
+
+          <meta property="og:type" content="website"></meta>
+          <meta property="og:url" content="https://linablidi.netlify.app/" />
+          <meta property="og:title" content="Lina Blidi Portfolio"></meta>
+          <meta property="og:description" content="Hey, I'm Lina Blidi a UI UX Designer, student and freelance from Paris :relaxed:"></meta>
+          <meta property="og:image" content={cover} />
+
+          <meta property="twitter:card" content="summary_large_image"></meta>
+          <meta property="twitter:url" content="https://linablidi.netlify.app/" />
+          <meta property="twitter:title" content="Lina Blidi Portfolio"></meta>
+          <meta property="twitter:description" content="Hey, I'm Lina Blidi a UI UX Designer, student and freelance from Paris :relaxed:"></meta>
+          <meta property="twitter:image" content={cover} />
+        </Helmet>
         <Alert AlertText='🚧 WORK IN PROGRESS 🚧 ' AlertType='danger' />
         <Header />
         <Switch>
